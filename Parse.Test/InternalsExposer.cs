121051c391bcd0af4bfc20060915dfae79469cf0
Alex Fanat <alex.fanat@smus.ca>
2018-12-27 22:13:52 -0800

    Add better support for exposing internal members of the Parse project source to the Parse.Test project source, and add MSBuild configuration for building unique time-based development versions of the Parse assembly.

---
+++ Parse.Test/InternalsExposer.cs
@@ -0,0 +1,12 @@
+[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo("Parse")]
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class IgnoresAccessChecksToAttribute : Attribute
+    {
+        public string AssemblyName { get; }
+
+        public IgnoresAccessChecksToAttribute(string assemblyName) => AssemblyName = assemblyName;
+    }
+}
